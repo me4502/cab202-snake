3706c8e39d1ef674ad711a86d52630587aea1291
Me4502 <mnmiller1@hotmail.com>
2016-10-05 18:08:39 +1000

    Started the base of the game. Added the intro text.

--- snake.c
+++ snake.c
@@ -1,5 +1,49 @@
 #include "snake.h"
 
 int main() {
-   return 0;
+    set_clock_speed(CPU_8MHz);
+
+    lcd_init(LCD_DEFAULT_CONTRAST);
+
+    while (1) {
+        update();
+
+        render();
+
+        _delay_ms(50);
+    }
+
+    return 0;
+}
+
+void update() {
+
+}
+
+void render() {
+    // Drawing
+    clear_screen();
+
+    if (gamestate == GAMESTATE_OPENING) {
+        for (int i = 0; i < sizeof(intro_text) / sizeof(intro_text[0]); i++) {
+            char * text = intro_text[i];
+
+            int x = LCD_X / 2 - (string_length(text) * 5) / 2; // Glyphs have a width of 5
+
+            draw_string((unsigned char) x, (unsigned char) (9 * 2 + i * 9), text);
+        }
+        show_screen();
+        _delay_ms(2000); // Delay for 2 seconds. // TODO Make this a little neater. Timers etc
+        gamestate = GAMESTATE_PLAYING;
+    }
+
+    show_screen();
+}
+
+int string_length(char * str) {
+    int length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
+    return length;
 }
--- snake.h
+++ snake.h
@@ -1,1 +1,39 @@
 #pragma once
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
+#include <avr/interrupt.h>
+
+#include "lcd.h"
+#include "graphics.h"
+#include "cpu_speed.h"
+#include "sprite.h"
+
+typedef struct snakeSegment {
+    int x;
+    int y;
+} SnakeSegment;
+
+int length = 2;
+int lives = 5;
+
+char * intro_text[] = {
+        "By Madeline Miller",
+        "n9342401"
+};
+
+// Game State
+#define GAMESTATE_OPENING 0
+#define GAMESTATE_PLAYING 1
+#define GAMESTATE_GAMEOVER 2
+
+unsigned char gamestate = GAMESTATE_OPENING;
+
+// Method Declarations
+void update();
+
+void render();
+
+// Utility Functions
+int string_length(char * str);
